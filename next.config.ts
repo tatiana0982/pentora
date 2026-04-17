d8dafd5fa187c5bc2451f3cd790aeb7c455c082d
tatiana0982 <vermakiideas@gmail.com>
2026-04-17 05:32:11 +0000

    Fixed next configs

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,10 @@ const nextConfig: NextConfig = {
   },
     /* config options here */
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
 };
 export default nextConfig;
